9c2413cdf5350127b3c4e7604271fdd729215d62
FernandoRafaeel15 <78438623+FernandoRafaeel15@users.noreply.github.com>
2021-03-19 21:29:09 -0600

    Add files via upload

---
+++ Practica 3/script de powershell.ps1
@@ -0,0 +1,17 @@
+﻿function test{
+   param(
+   [int]$numero
+   )
+   While($numero -lt 100){
+   if($numero -eq 98){
+   
+   Write-Host "escribir"   
+   }
+   Write-Host "xd"
+   $numero++
+   }
+
+}
+
+
+test 16
